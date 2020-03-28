0311e59546e48d2614fc2ddb92f1503bd8349a83
Esther Ibáñez <eibanez@ces.vocento.com>
2020-03-28 11:15:24 +0100

    Fix encore system with adminlte

--- assets/adminlte-demo.js
+++ assets/adminlte-demo.js
@@ -1,5 +1,13 @@
 require('../vendor/kevinpapst/adminlte-bundle/Resources/assets/admin-lte');
 
+require('datatables.net-dt');
+// require('popper')
+// require('child_process')
+// require('fs')
+
+require('bootstrap')();
+require( 'datatables.net-bs4' )();
+
 // ------ for charts ------
 const Chart = require('chart.js/dist/Chart.min');
 global.Chart = Chart;
